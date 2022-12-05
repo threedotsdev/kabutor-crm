ed4ff6c8a6267871155ef8e82300114de34a60c7
Rokibul Hasan <therokibul@gmail.com>
2022-12-05 08:50:06 +0600

    color palette update

--- lib/packages.dart
+++ lib/packages.dart
@@ -2,3 +2,7 @@ export 'package:firebase_core/firebase_core.dart';
 export 'package:flutter/material.dart';
 export 'package:flutter/services.dart';
 export 'package:kabutor/firebase_options.dart';
+
+
+export 'utils/palette.dart';
+export 'utils/constans.dart';
---
+++ lib/utils/palette.dart
@@ -0,0 +1,20 @@
+import '../packages.dart';
+
+class Palette {
+  static const Color scaffold = Color(0xFFEDEDED);
+  static const Color themeColor = Color(0xff00A784);
+  static const Color greyColor = Color(0xff8D8E8F);
+  static const Color darkColor = Color(0xff24292E);
+  static const Color darkLightColor = Color(0xff2F363D);
+  static const Color redColor = Color(0xffFF6767);
+
+  static const LinearGradient scaffoldGradient = LinearGradient(
+    colors: [
+      Color(0xFFFFFFFF),
+      Color(0xFFF5FFFD),
+      Color(0xffEAF6F5),
+      Color(0xffDBEEEC),
+      Color(0xffCAFFF4),
+    ],
+  );
+}
