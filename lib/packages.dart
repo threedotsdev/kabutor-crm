ea6e1a070275e672471ab5b2153cc0f691fbe57f
arafathossainmahe <54477175+arafathossainmahe@users.noreply.github.com>
2022-12-07 09:14:32 +0600

    welcomeScreen

--- lib/packages.dart
+++ lib/packages.dart
@@ -2,7 +2,11 @@ export 'package:firebase_core/firebase_core.dart';
 export 'package:flutter/material.dart';
 export 'package:flutter/services.dart';
 export 'package:kabutor/firebase_options.dart';
+export 'package:flutter_screenutil/flutter_screenutil.dart';
+export 'package:get/get.dart';
 
 
 export 'utils/palette.dart';
 export 'utils/constans.dart';
+export '/ui/shared/widgets/custom_button.dart';
+export 'ui/pages/auth/signin.dart';
---
+++ lib/ui/pages/auth/signin.dart
@@ -0,0 +1,240 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../packages.dart';
+
+class SignIn extends StatelessWidget {
+  const SignIn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    ScreenUtil.init(
+      context,
+      designSize: const Size(390, 844),
+    );
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Container(
+            width: 390.w,
+            height: 844.h,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.white,
+                  Color(0xfff4fffc),
+                  Color(0xffeaf6f5),
+                  Color(0xffdbeeec),
+                  Color(0xffcafff4),
+                ],
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 39, left: 22),
+                  child: SizedBox(
+                    height: 70.h,
+                    width: 210.w,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Log in or Sing up",
+                          style: TextStyle(
+                            color: const Color(0xff2f363d),
+                            fontSize: 25.sp,
+                            //fontFamily: "Lato",
+                            //fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Text(
+                          "Hello World\n",
+                          style: TextStyle(
+                            color: const Color(0xff00a784),
+                            fontSize: 16.sp,
+                            //fontFamily: "Lato",
+                            //fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 108.h),
+                Padding(
+                  padding: const EdgeInsets.only(left: 22, right: 22),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.h,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 22, right: 22),
+                  child: Container(
+                    height: 54.84.h,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Color(0x3f000000),
+                          blurRadius: 4,
+                          offset: Offset(0, 4),
+                        ),
+                      ],
+                      color: Color(0xff00a784),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        "CONTINUE",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          //fontFamily: "Lato",
+                          //fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 28.16),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 22,
+                    right: 22,
+                  ),
+                  child: SizedBox(
+                    width: 342.w,
+                    height: 12.h,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          color: Colors.black,
+                          width: 161.w,
+                          height: 1.h,
+                        ),
+                        SizedBox(width: 2.50),
+                        const Text(
+                          "OR",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Color(0x7f2f363d),
+                            fontSize: 10,
+                          ),
+                        ),
+                        SizedBox(width: 2.50),
+                        Container(
+                          color: Colors.black,
+                          width: 161.w,
+                          height: 1.w,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 28.16),
+                Padding(
+                  padding: const EdgeInsets.only(left: 22,right: 22),
+                  child: Container(
+                    width: 346.w,
+                    height: 54.84.h,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(
+                        color: Color(0xff2f363d),
+                        width: 1,
+                      ),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Color(0x3f000000),
+                          blurRadius: 4,
+                          offset: Offset(0, 4),
+                        ),
+                      ],
+                      //color: Palette.themeColor,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20.23),
+                SizedBox(
+                  width: 346.w,
+                  height: 54.84.h,
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 346,
+                        height: 54.84,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          border: Border.all(
+                            color: Color(0xff3b3e51),
+                            width: 1,
+                          ),
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Color(0x3f000000),
+                              blurRadius: 4,
+                              offset: Offset(0, 4),
+                            ),
+                          ],
+                          color: Color(0x003b3d51),
+                        ),
+                        child: Stack(
+                          children: [
+                            const Positioned.fill(
+                              child: Align(
+                                alignment: Alignment.centerRight,
+                                child: SizedBox(
+                                  width: 317.14,
+                                  height: 29.42,
+                                  child: Text(
+                                    "Google",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontFamily: "Lato",
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              left: 29,
+                              top: 9.93,
+                              child: Container(
+                                width: 34,
+                                height: 34,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                child: FlutterLogo(size: 34),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
